cf44f1810a4b9b2a0fee2b24686e32d425a6c7e4
Ismail Zare <zarehesmaiel@gmail.com>
2022-08-13 18:28:57 +0430

    fix display name of user field

--- src/Nova/Filters/Subscriber.php
+++ src/Nova/Filters/Subscriber.php
@@ -38,7 +38,7 @@ class Subscriber extends Filter
     {
         return User::newModel()->get()->map(function($user) {
             return [
-                'name' => $user->fullname(),
+                'name' => $user->fullname() ?? $user->name,
                 'value' => $user->getKey(),
             ];
         });
